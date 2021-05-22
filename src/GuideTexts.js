1c41adafcfe2bc8f33aa35f187e3de3a8bc9a1de
silverkip <46242446+silverkip@users.noreply.github.com>
2021-05-22 22:54:11 +0900

    Update guide

--- src/GuideTexts.js
+++ src/GuideTexts.js
@@ -7,7 +7,7 @@ const guideTexts = [
   {title:"Step 6", img:'/img/step5.jpg', instruction:"Fold the corner across the edge"},
   {title:"Step 7", img:'/img/step6.jpg', instruction:"Turn over and fold the corner into the back of the edge"},
   {title:"Step 8", img:'/img/step7.jpg', instruction:"Fold it along the center line"},
-  {title:"You did it!", img:'/img/step8.jpg', instruction:"Easy isn't it?"},
+  {title:"You did it!", img:'/img/step8.jpg', instruction:"Give yourself a pat on the back"},
   {title:"Why stop at one?", img:'/img/step9.jpg', instruction:"Keep making more and you'll get better at it!"},
 ];
 
